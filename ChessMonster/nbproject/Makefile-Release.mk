58b7a2df71563b4332c9d67f18955f8a527dcd86
Timo <timo@no.mail>
2013-09-20 22:06:16 +0300

    Updated project configuration.

--- ChessMonster/nbproject/Makefile-Release.mk
+++ ChessMonster/nbproject/Makefile-Release.mk
@@ -65,7 +65,7 @@ ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/chessmonster: ${OBJECTFILES}
 ${OBJECTDIR}/main.o: main.cpp 
 	${MKDIR} -p ${OBJECTDIR}
 	${RM} $@.d
-	$(COMPILE.cc) -O2 -DNDEBUG -std=c++11 -Wall -pedantic-errors -march=corei7 -MMD -MP -MF $@.d -o ${OBJECTDIR}/main.o main.cpp
+	$(COMPILE.cc) -O2 -DNDEBUG -std=c++11 -MMD -MP -MF $@.d -o ${OBJECTDIR}/main.o main.cpp
 
 # Subprojects
 .build-subprojects:
