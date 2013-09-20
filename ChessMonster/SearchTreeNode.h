d2bc2bbdba7187a5fc1418a9e21223be2c4e93e7
Timo <timo@no.mail>
2013-09-21 01:56:37 +0300

    Dummy classes for SearchTreeNode and TreeGeneretor.

---
+++ ChessMonster/SearchTreeNode.h
@@ -0,0 +1,12 @@
+#pragma once
+
+namespace cm {
+
+class SearchTreeNode
+{
+public:
+
+	SearchTreeNode(...) { }
+};
+
+}
---
+++ ChessMonster/TreeGenerator.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "SearchTreeNode.h"
+
+namespace cm {
+
+class TreeGenerator
+{
+public:
+
+	TreeGenerator(unsigned maxDepth) { }
+
+	void clear() { }
+
+	void startNode(...) { }
+
+	void endNode(...) { }
+	
+	SearchTreeNode getTree()
+	{
+		return SearchTreeNode(); //TODO
+	}
+};
+
+}
