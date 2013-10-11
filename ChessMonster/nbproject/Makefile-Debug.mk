ef833f02f7534bcca34bf4914fb8e97024b8ddeb
Timo <timjlah@gmail.com>
2013-10-11 16:51:13 +0300

    Reverted netbeans project back to version 7.3.1 + some minor changes.
    - Default architecture mode for release config.
    - NDEBUG=1 instead of NDEBUG

--- ChessMonster/nbproject/Makefile-Debug.mk
+++ ChessMonster/nbproject/Makefile-Debug.mk
@@ -65,7 +65,7 @@ ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/chessmonster: ${OBJECTFILES}
 ${OBJECTDIR}/src/main.o: src/main.cpp 
 	${MKDIR} -p ${OBJECTDIR}/src
 	${RM} $@.d
-	$(COMPILE.cc) -g -std=c++11 -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/main.o src/main.cpp
+	$(COMPILE.cc) -g -std=c++11 -Wall -pedantic-errors -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/main.o src/main.cpp
 
 # Subprojects
 .build-subprojects:
